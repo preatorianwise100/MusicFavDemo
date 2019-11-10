66754e30e1380e62cbd7588118cbfd4c12056fd4
preatorianwise999 <preatorianwise999@hotmail.com>
2019-11-09 21:38:34 -0300

    tableview favoritos details

---
+++ MusicFav/ViewControllers/ViewMusic/VCDetailsMusic.swift
@@ -0,0 +1,20 @@
+//
+//  VCDetailsMusic.swift
+//  MusicFav
+//
+//  Created by Jose David Bustos H on 11/9/19.
+//  Copyright © 2019 Jose David Bustos H. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class VCDetailsMusic: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+}
